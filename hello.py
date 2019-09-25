eae86a815b4976e964693a663af38677251a8fd6
objectprogr <objectprogr@gmail.com>
2019-09-25 15:15:33 +0200

    Added comments to code for better explain

--- hello.py
+++ hello.py
@@ -11,9 +11,18 @@ def web(page, WebUrl):
         code = requests.get(url)
         plain = code.content
         s = BeautifulSoup(plain, "html.parser")
-        for value in s.find_all(class_='archive__item-title'):
-            #print(value.get_text())
+        for value in s.find_all(class_='archive__item-title'):            
+            # [EN] strip() remove new lines from results
+            # ---
+            # [PL] strip() usuwa nam wszystkie nowe linie w wynikach
             value = value.get_text().strip()
+            # [EN] we adding new line after remove new lines above, because
+            # in this case we dont`t have empty lines between results
+            # ---
+            # [PL] dodajemy w tym miejscu nowa linie po kazdym znalezionym elemencie
+            # poniewaz, wynik jest w ladniejszej formie, tzn. nie mamy pustych linii
+            # pomiedzy wynikami. Gdybysmy wyzej nie usuneli wszystki nowych linii i nie dodali
+            # w tym miejscu nowych, to mielibysmy cos takiego: wynik pusta linia wynik pusta linia
             f.write(value + '\n')
 
 web(1,'https://objectprogr.github.io/')
